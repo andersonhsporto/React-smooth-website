eb7c1e3f9d6eb6cc244ee2d1f2268c7e16b43fc2
andersonhsporto <andersonhsporto@users.noreply.github.com>
2022-07-16 02:06:16 +0000

    Apply formatting changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-		<Navbar />
+      <Navbar />
     </Router>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
 
 const Navbar = () => {
-	return (
-		<Nav>
-			<NavbarContainer>
-				<NavLogo to="/">teste</NavLogo>
-				<h1>teste</h1>
-			</NavbarContainer>
-		</Nav>
-	);
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">teste</NavLogo>
+        <h1>teste</h1>
+      </NavbarContainer>
+    </Nav>
+  );
 };
 
 export default Navbar;

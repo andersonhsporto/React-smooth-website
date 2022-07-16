db52bb5b72686e13fa9ae60dfad1b42ddb6588ae
Anderson Porto <anderson.higo2@gmail.com>
2022-07-15 23:10:17 -0300

    new: test

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -4,7 +4,7 @@ import { Link as LinkR } from "react-router-dom";
 
 export const Nav = styled.nav`
   background: green;
-  height: 80px;
+  		height: 80px;
   //   margin-top: -80px;
   display: flex;
   justify-content: center;
